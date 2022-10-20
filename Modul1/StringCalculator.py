56e9a2eb1827e1386a425a1e5ca87c3903e3b175
pbaszyn <61763299+pbaszyn@users.noreply.github.com>
2022-10-20 17:10:18 +0200

    String calculator - new line check - in progress

--- Modul1/StringCalculator.py
+++ Modul1/StringCalculator.py
@@ -1,7 +1,36 @@
+import math
+
+
 class StringCalculator:
     def add(self, *stringnumbers):
         sum = 0
         for string in stringnumbers:
             if string:
-                sum += int(string)
-        return str(sum)
+                if self.checkForNewLineSeparator(string) == -1:
+                    return ''
+
+                sum += float(string)
+        return self.getStringResult(sum)
+
+    def getStringResult(self, numberInFloatFormat):
+        fractionalPartOfFloat = math.modf(numberInFloatFormat)[0]
+        if fractionalPartOfFloat > 0:
+            stringToReturn = str(numberInFloatFormat)
+        else:
+            stringToReturn = str(int(numberInFloatFormat))
+        return stringToReturn
+
+    def checkForNewLineSeparator(self, stringToCheck):
+#        if stringToCheck.endswith('\n'):
+#            return -1
+        newLineIncorrectPosition = stringToCheck.find(',\n')
+        if newLineIncorrectPosition > -1:
+            sumOfSeparatedNumbers = 0.0
+            listOfNumbers = stringToCheck.split('\n')
+            for number in listOfNumbers:
+                if number:
+                    sum += float(number)
+            return sumOfSeparatedNumbers
+        else:
+            #return wrong possition.
+
--- Modul1/test_StringCalculator.py
+++ Modul1/test_StringCalculator.py
@@ -10,7 +10,15 @@ class TestStringCalculator(TestCase):
 
     def testIfReturnsSumOfNumbersIfTwoNumbersAreGiven(self):
         calculator = StringCalculator()
-        self.assertEqual('4', calculator.add('2', '2'))
+        self.assertEqual('4', calculator.add('2.0', '2'))
+
+    def testIfReturnsSumOfNumbersIfTwoFloatNumbersAreGiven(self):
+        calculator = StringCalculator()
+        self.assertEqual('4.5', calculator.add('2.2', '2.3'))
+
+    def testIfReturnsSumOfNumbersIfFourFloatAndIntegerNumbersAreGiven(self):
+        calculator = StringCalculator()
+        self.assertEqual('7.5', calculator.add('2.2', '2.3', '1', '2.0'))
 
     def testIfReturnsZeroIfEmptyStringIsGiven(self):
         calculator = StringCalculator()
